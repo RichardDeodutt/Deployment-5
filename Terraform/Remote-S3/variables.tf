6359375f7a851b2b21b3f88ad944cba2329962a3
Richard Deodutt <richarddeodutt@gmail.com>
2022-11-14 19:20:07 -0500

    Removed defaults because it's not overriding

--- Terraform/Remote-S3/variables.tf
+++ Terraform/Remote-S3/variables.tf
@@ -1,14 +1,11 @@
 variable "region"{
     type = string
-    default = "ap-northeast-1"
 }
 
 variable "bucketname"{
     type = string
-    default = "terraform-remote-statefile-store-d5"
 }
 
 variable "tablename"{
     type = string
-    default = "terraform_state_lock_table-d5"
 }
