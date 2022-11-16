5b457b236b69dcefa3fe9faa730c06f2a8b32d92
Richard Deodutt <richarddeodutt@gmail.com>
2022-11-15 21:10:55 -0500

    Fix agent

--- Scripts/installagent.sh
+++ Scripts/installagent.sh
@@ -29,21 +29,6 @@ main(){
 
     #Install java if not already
     aptinstalllog "default-jre"
-
-    #Install nginx if not already
-    aptinstalllog "nginx"
-
-    #Install python3-gunicorn if not already
-    aptinstalllog "python3-gunicorn"
-
-    #Install curl if not already
-    aptinstalllog "curl"
-
-    #Download and set the nginx configuration
-    curl -s $ConfigNginx | tee /etc/nginx/sites-enabled/default > /dev/null 2>&1 && logokay "Successfully Set Nginx" || { logerror "Failure Setting Nginx" && exiterror ; }
-
-    #Restart the nginx service
-    systemctl restart nginx && logokay "Successfully restarted nginx" || { logerror "Failure restarting nginx" && exiterror ; }
 }
 
 #Log start
